6d616ae1427f8a31c53c200301f900c1959e263f
pmahend1 <prateek.mahendrakar@gmail.com>
2017-03-18 01:01:09 -0400

    added ListViewDemo

---
+++ ListViewDemo/app/src/main/java/com/uncc/prateek/listviewdemo/CustomAdapter.java
@@ -0,0 +1,14 @@
+package com.uncc.prateek.listviewdemo;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Prateek on 20-02-2017.
+ */
+
+public class CustomAdapter extends ArrayAdapter {
+    public CustomAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+}
---
+++ ListViewDemo/app/src/main/java/com/uncc/prateek/listviewdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.uncc.prateek.listviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView lv = (ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String>
+    }
+}
